bc6ef4be5d7ca618f6fae41dbcd2bc9f4552accb
AryanNegi05 <negiaryan13205@gmail.com>
2025-06-26 13:13:59 +0530

    First commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/database.js'
+import authRouter from './routes/authRoutes.js'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import userRouter from './routes/userRoutes.js'
+import messageRouter from './routes/messageRoutes.js'
+import { app, server } from './socket/socket.js'
+dotenv.config()
+
+const port=process.env.PORT || 5000
+
+
+app.use(cors({
+    origin:"*",
+    credentials:true
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+app.use("/api/message",messageRouter)
+
+server.listen(port,()=>{
+    connectDb()
+    console.log(`Server Started at ${port}`)
+})
---
+++ frontend/src/config.jsx
@@ -0,0 +1,1 @@
+export const serverUrl = "https://chatly-qc4w.onrender.com";
