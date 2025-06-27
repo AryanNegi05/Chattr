4dcda955988d0cb3ce1298bd83f567b3a3b52493
AryanNegi05 <negiaryan13205@gmail.com>
2025-06-27 12:09:59 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ let app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
-        origin:"https://chattr-six.vercel.app"
+        origin:"*"
     }
 })
  const userSocketMap = {}
