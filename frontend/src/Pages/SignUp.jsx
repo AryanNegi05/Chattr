51c7ee740a848869f79f8181cafcab3679156c57
AryanNegi05 <negiaryan13205@gmail.com>
2025-09-10 05:25:57 +0530

    Update SignUp.jsx

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -57,7 +57,7 @@ function SignUp() {
                 <path fillRule='evenodd' d='M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z' clipRule='evenodd' />
               </svg>
             </div>
-            <h1 className='text-3xl font-bold text-white mb-2 animate-in slide-in-from-left-4 duration-700 delay-500'>Join Chatly</h1>
+            <h1 className='text-3xl font-bold text-white mb-2 animate-in slide-in-from-left-4 duration-700 delay-500'>Join Chattr</h1>
             <p className='text-indigo-100 text-sm animate-in slide-in-from-right-4 duration-700 delay-700'>Create your account to get started</p>
           </div>
         </div>
